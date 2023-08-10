c028b193a2457b3baeae0635d1d012465f68bdb8
Gihan Rangana <itsmerangana@gmail.com>
2023-08-10 15:55:57 +0530

    remove everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import Accordion from "./components/Accordion/Accordion";
-import useAccordion from "./hooks/useAccordion";
-
 function App() {
-
-    const steps = [
-        {
-            name: "acc1",
-            title: "Accordion 1",
-            render: () => <p>Accordion 1 content</p>,
-        },
-        {
-            name: "acc2",
-            title: "Accordion 2",
-            render: () => <p>Accordion 2 content</p>,
-        }
-    ]
-
-    const accordion: any = useAccordion(steps, 'acc2');
 
     return (
         <div className="App">
-            <Accordion {...accordion}/>
+            <h1>Let's Start Creating Awesome useAccordion hook</h1>
         </div>
     )
 }
